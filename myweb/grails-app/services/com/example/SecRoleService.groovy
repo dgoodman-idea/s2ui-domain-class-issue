4f23593db84b8916d086d6bb92a5e008348f8ff5
David Goodman <david.goodman@ideaedu.org>
2019-03-19 15:28:53 -0600

    Initial commit

---
+++ myweb/grails-app/services/com/example/SecRoleService.groovy
@@ -0,0 +1,18 @@
+package com.example
+
+import grails.gorm.services.Service
+
+@Service(SecRole)
+interface SecRoleService {
+
+    SecRole get(Serializable id)
+
+    List<SecRole> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    SecRole save(SecRole secRole)
+
+}
---
+++ myweb/grails-app/services/com/example/SecUserSecRoleService.groovy
@@ -0,0 +1,18 @@
+package com.example
+
+import grails.gorm.services.Service
+
+@Service(SecUserSecRole)
+interface SecUserSecRoleService {
+
+    SecUserSecRole get(Serializable id)
+
+    List<SecUserSecRole> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    SecUserSecRole save(SecUserSecRole secUserSecRole)
+
+}
